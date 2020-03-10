416713b3462d53215484dffa2c4bb955284c57be
ASuicidalCow <aaron.bertagnole@gmail.com>
2020-03-10 17:31:17 -0600

    added initial files

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+        
+    var 
+    var guessCount = 0;
+    var MAX_GUESSES =8;
+    var game = ["Wyoming", "Arizona", "Utah", "Idaho" ]
+    var randomWord = Math.floor(Math.random()*4);
+    var answer[choice];
+    var myLength = answer.length;
+    
+
+    alert(answer);
+    
+    function guess(){
+
+    }
+    
