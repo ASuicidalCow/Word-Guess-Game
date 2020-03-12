6e458db97f9eac12c4892133140cc50626359021
ASuicidalCow <aaron.bertagnole@gmail.com>
2020-03-12 17:09:07 -0600

    updated with some logic and variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,47 @@
-        
-    var 
-    var guessCount = 0;
-    var MAX_GUESSES =8;
-    var game = ["Wyoming", "Arizona", "Utah", "Idaho" ]
-    var randomWord = Math.floor(Math.random()*4);
-    var answer[choice];
-    var myLength = answer.length;
-    
+window.onload = function () {
 
-    alert(answer);
-    
-    function guess(){
+  var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n',
+  'o','p','q','r','s','t','u','v','w','x','y','z'];
 
+  var states;            // different states
+  var getHint;           // get hint for word
+  var word;              // selected word
+  var getGuess;          // get the guess
+  var geusses = [];      // stored guesses
+  var lives;             // how many tries left
+  var counterWins;       // counter for wins
+  var counterLosses;     // counter for losses
+  var spacesInWord;      // number of spaces in a word
+
+
+  // get the elements
+  var showlives = document.getElementById("#guessesLeft");
+  var getHint = document.getElementById("#hint");
+  var showClue = document.getElementById("#clue");
+
+
+  
+  var buttons = function() {
+    myButtons = document.getElementById("buttons");
+    letters = document.createElement("ul");
+    for (var i = 0; i < alphabet.length; i++) {
+      letters.id = 'alphabet';
+      list = document.createElement("li");
+      list.id = 'letter';
+      list.innerHTML = alphabet[i];
+      check();
+      myButtons.appendChild(letters);
+      letters.appendChild(list);
     }
-    
+
+
+
+  }
+
+
+
+
+
+
+
+  }
