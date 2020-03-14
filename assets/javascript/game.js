cfc5e3292f4657d907da3ff6db106783e7d352ce
ASuicidalCow <aaron.bertagnole@gmail.com>
2020-03-14 17:35:37 -0600

    re wrote a lot of code over. added a bunch of new variables.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,36 @@
-window.onload = function () {
 
-  var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n',
-  'o','p','q','r','s','t','u','v','w','x','y','z'];
-
-  var states;            // different states
-  var getHint;           // get hint for word
-  var word;              // selected word
-  var getGuess;          // get the guess
-  var geusses = [];      // stored guesses
-  var lives;             // how many tries left
-  var counterWins;       // counter for wins
-  var counterLosses;     // counter for losses
-  var spacesInWord;      // number of spaces in a word
-
-
-  // get the elements
-  var showlives = document.getElementById("#guessesLeft");
-  var getHint = document.getElementById("#hint");
-  var showClue = document.getElementById("#clue");
-
-
+// array of letters
+  var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  // variables for tracking the wins and losses 
+  var wins = 0;
+  var losses = 0;
+  var guesses = 10;
   
-  var buttons = function() {
-    myButtons = document.getElementById("buttons");
-    letters = document.createElement("ul");
-    for (var i = 0; i < alphabet.length; i++) {
-      letters.id = 'alphabet';
-      list = document.createElement("li");
-      list.id = 'letter';
-      list.innerHTML = alphabet[i];
-      check();
-      myButtons.appendChild(letters);
-      letters.appendChild(list);
-    }
+   var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+   console.log(computerGuess)
 
-
-
-  }
-
-
-
-
-
-
-
-  }
+   //when the user presses a key it runs the function
+   document.onkeypress = function(event){
+     var userGuess = event.key;
+     if (userGuess === computerGuess) {
+       win();
+     } else if(guesses - 1 === 0){
+       lost();
+     }else{
+       fail(guess);
+     }
+      display();
+     }
+      function display(){
+        var winsP = document.getElementById("wins");
+        var lossesP =  document.getElementById("losses");
+        var guesses = document.getElementById("guesses");
+        winsP.innerHTML = wins;
+        lossesP.innerHTML =  losses;
+        guesses.innerHTML =  guesses;
+        document.getElementById("lettersGuessed").innerHTML = lettersGuessed.join(",");
+      
+      }
+    
