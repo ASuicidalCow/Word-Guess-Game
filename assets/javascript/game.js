654db342199c158ffb0b1897c39d66395a1b13ef
ASuicidalCow <aaron.bertagnole@gmail.com>
2020-03-14 19:58:26 -0600

    went through multiple changes had issues with it showing the same letter everytime.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,34 +3,45 @@
   var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  // variables for tracking the wins and losses 
+  // variables for tracking the wins and losses, and guesses
   var wins = 0;
   var losses = 0;
-  var guesses = 10;
-  
-   var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-   console.log(computerGuess)
-
+  var guessesLeft = 10;
+  var guessedLetters = [];
+  var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+    console.log(computerGuess)
+  var restart = function(){
+    guessesLeft = 10;
+    guessedLetters = [];
+    computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+    console.log(computerGuess)
+  }
    //when the user presses a key it runs the function
    document.onkeypress = function(event){
-     var userGuess = event.key;
+     var userGuess = event.key.toLowerCase();
+    guessedLetters.push(userGuess);
      if (userGuess === computerGuess) {
-       win();
-     } else if(guesses - 1 === 0){
-       lost();
-     }else{
-       fail(guess);
-     }
-      display();
-     }
-      function display(){
-        var winsP = document.getElementById("wins");
-        var lossesP =  document.getElementById("losses");
-        var guesses = document.getElementById("guesses");
-        winsP.innerHTML = wins;
-        lossesP.innerHTML =  losses;
-        guesses.innerHTML =  guesses;
-        document.getElementById("lettersGuessed").innerHTML = lettersGuessed.join(",");
+      wins++;
+      guessesLeft = 9;
+      alert("You Won!")
+      restart();
       
+     }
+      else{
+        guessesLeft--;
+        
       }
-    
+      if(guessesLeft === 0){
+        losses++;
+        guessesLeft = 9;
+        alert("Sorry you lost :(")
+        restart();
+      }
+      
+      document.getElementById("wins").innerHTML = "Wins: " + wins;
+      document.getElementById("losses").innerHTML = "losses: " + losses;
+      document.getElementById("guesses").innerHTML = "guesses: " + guessesLeft;
+      document.getElementById("lettersGuessed").innerHTML = "Letters Guessed: " + guessedLetters;
+      
+     }
+     
